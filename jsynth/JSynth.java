8a80e6c858e945ba02e8007376e5f03d2ef951e0
Atsushi Yonenaga <a.yonenaga@gmail.com>
2012-07-27 22:24:42 +0900

    runs without PlaySquareWave.class

--- jsynth/JSynth.java
+++ jsynth/JSynth.java
@@ -3,25 +3,25 @@
 //
 
 /**
- *
- *
+ * FMな音のするシンセになる予定。
+ * 複数のオシレータをもつ。
  *
  */
-package jsynth;
+package fursynth;
 
 import javax.sound.sampled.*;
 
-public class JSynth{
+public class FurSynth{
 	static int C=0,CS=1,D=2,DS=3,E=4,F=5,FS=6,G=7,GS=8,A=9,AS=10,B=11;
 	private int BPM = 120;
-	private int SAMPLE_RATE = 44100;
-	//周波数を計算してテーブルにセットする
+	private int sampleRate = 44100;
 	static double note_freq[] = new double [128];
-	  
-	static int getNoteNo(int octave, int noteName){
-		return octave * 12 + noteName;//音名からNoteNoを計算する
-	}
-	static void calcFrequency(){
+
+	/**
+	 * 周波数テーブルを生成
+	 * 
+	 */
+	private static void calcFrequency(){
 		//半音(2の12乗根)を計算
 		double r = Math.pow(2.0, 1.0 /12.0);
 		// A(NoteNo=69)より上の音を計算
@@ -39,26 +39,38 @@ public class JSynth{
 		}
 		return;
 	}
+
+
+	 /**
+	  * 音名からNoteNoを計算する
+	  */
+	static int getNoteNo(int octave, int noteName){
+		return octave * 12 + noteName;
+	}
+
+	/**
+	 * ノート番号から周波数を引く
+	 */
 	static double getNoteNoToFreq(int octave, int noteName) {
 		return note_freq[getNoteNo(octave, noteName)];
 	}
+
 	/**
 	 * n分音符の長さを計算する
 	 * @param n分音符
 	 */
 	int getSampleCount(int nLength){
-		double n4 = (60.0 / this.BPM) * SAMPLE_RATE; //四分音符のサンプル数
+		double n4 = (60.0 / this.BPM) * sampleRate; //四分音符のサンプル数
 		double n1 = n4 * 4; //全音符のサンプル数
 		return (int)Math.round(n1 / nLength);
 	}
 
-	public void showName(){
-		System.out.println("package JSynth");
-	}
-
+	/**
+	 * main関数
+	 */
 	public static void main(String[] args){
-		JSynth js = new JSynth();
-		PlaySquareWave myosc = new PlaySquareWave();
+		FurSynth fs = new FurSynth();
+		Oscilator myosc = new Oscilator(js.sampleRate);
 		calcFrequency();
 
 		int L2 = js.getSampleCount(2);
